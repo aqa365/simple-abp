aab3710a8f1cb0e0114ccb630ad54abec86c7550
aqa365 <aqa.china@outlook.com>
2022-01-13 18:27:55 +0800

    处理语言内容

--- src/Simple.Abp.Test.Host/SimpleTestHttpApiHostModule.cs
+++ src/Simple.Abp.Test.Host/SimpleTestHttpApiHostModule.cs
@@ -185,16 +185,14 @@ namespace Simple.Abp.Test
         {
             Configure<AbpLocalizationOptions>(options =>
             {
-                options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
-                options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
-                options.Languages.Add(new LanguageInfo("en", "en", "English"));
-                options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
-                options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
-                options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
-                options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
-                options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
-                options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-                options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
+                options.Languages.Add(new LanguageInfo("ar", "ar", "العربية","ar.svg"));
+                options.Languages.Add(new LanguageInfo("en", "en", "English","en.svg"));
+                options.Languages.Add(new LanguageInfo("fi", "fi", "Finnish","fi.svg"));
+                options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe","tr.svg"));
+                options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文", "zh-Hans.svg"));
+                options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文", "zh-Hans.svg"));
+                options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch", "de.svg"));
+                options.Languages.Add(new LanguageInfo("es", "es", "Español","es.svg"));
             });
         }
 
