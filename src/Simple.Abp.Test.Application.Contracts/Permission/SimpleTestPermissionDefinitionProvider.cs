f62693bf68375bfc21af5a77cbec2f1df77c2f58
aqa365 <aqa.china@outlook.com>
2022-03-22 17:48:15 +0800

    增加权限

---
+++ src/Simple.Abp.Test.Application.Contracts/Permission/SimpleTestPermissionDefinitionProvider.cs
@@ -0,0 +1,26 @@
+﻿using Volo.Abp.AuditLogging.Localization;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Localization;
+
+namespace Simple.Abp.Test.Permission
+{
+    public class SimpleTestPermissionDefinitionProvider : PermissionDefinitionProvider
+    {
+        public override void Define(IPermissionDefinitionContext context)
+        {
+            var group = context.AddGroup(SimpleTestPermissions.GroupName, L("Permission:Venom"));
+            group.AddPermission(SimpleTestPermissions.Venom.Default, L("Permission:Configs"));
+
+            group.AddPermission(SimpleTestPermissions.Venom.Aim, L("Permission:Aim"));
+            group.AddPermission(SimpleTestPermissions.Venom.Rcs, L("Permission:Rcs"));
+            group.AddPermission(SimpleTestPermissions.Venom.Radar, L("Permission:Radar"));
+            group.AddPermission(SimpleTestPermissions.Venom.Trigger, L("Permission:Trigger"));
+            group.AddPermission(SimpleTestPermissions.Venom.Sonar, L("Permission:Sonar"));
+        }
+
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<AuditLoggingResource>(name);
+        }
+    }
+}
---
+++ src/Simple.Abp.Test.Application.Contracts/Permission/SimpleTestPermissions.cs
@@ -0,0 +1,24 @@
+﻿using Volo.Abp.Reflection;
+
+namespace Simple.Abp.Test.Permission
+{
+    public class SimpleTestPermissions
+	{
+		public static string[] GetAll()
+		{
+			return ReflectionHelper.GetPublicConstantsRecursively(typeof(SimpleTestPermissions));
+		}
+
+		public const string GroupName = "Venom";
+
+		public class Venom
+		{
+			public const string Default = "Venom.Configs";
+			public const string Aim = "Venom.Aim";
+			public const string Rcs = "Venom.Rcs";
+			public const string Radar = "Venom.Radar";
+			public const string Trigger = "Venom.Trigger";
+			public const string Sonar = "Venom.Sonar";
+		}
+	}
+}
